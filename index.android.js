ea357fb40578db90c6c8a151d5546efa3abb64cb
unknown <Saiidma@gmail.com>
2017-08-05 14:22:11 +0100

    'Project'

---
+++ index.android.js
@@ -0,0 +1,19 @@
+// index.ios.js - place code in here for android !!
+
+//  Import a library to help create a Component
+import React from 'react';
+import { View, AppRegistry } from 'react-native';
+import Header from './src/components/header';
+import AlbumList from './src/components/AlbumList';
+
+// Create a Component
+const App = () => (
+  <View style={{ flex: 1 }}>
+    <Header headerText={'Albums'} />
+    <AlbumList />
+  </View>
+);
+
+// Render it to the device
+
+AppRegistry.registerComponent('albums', () => App);
